32c0db04f91762b5cfc1af996052717efdc17a47
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2025-06-04 21:46:35 +0200

    implementado viewmodel estadísticas

--- app/src/main/java/com/example/appstopsmoke/viewmodel/StatsViewModel.java
+++ app/src/main/java/com/example/appstopsmoke/viewmodel/StatsViewModel.java
@@ -1,4 +1,37 @@
 package com.example.appstopsmoke.viewmodel;
 
-public class StatsViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.appstopsmoke.data.model.Smoke;
+import com.example.appstopsmoke.data.repository.SmokeRepository;
+
+import java.util.List;
+
+public class StatsViewModel extends ViewModel {
+    private final SmokeRepository repository;
+    private final MutableLiveData<List<Smoke>> smokesData = new MutableLiveData<>();
+
+
+    // constructor que inicializa el viewmodel con el repositorio y carga los datos del usuario
+    public StatsViewModel(SmokeRepository repository) {
+        this.repository = repository;
+        loadUserData();
+    }
+
+
+    // carga los datos del usuario actual desde el repositorio
+    private void loadUserData() {
+        String userId = repository.getCurrentUserId();
+        if (userId != null) {
+            repository.getUserSmokes(userId);
+        }
+    }
+
+
+    // livedata con los datos del usuario
+    public LiveData<List<Smoke>> getSmokesData() {
+        return repository.getUserSmokes(repository.getCurrentUserId());
+    }
 }
