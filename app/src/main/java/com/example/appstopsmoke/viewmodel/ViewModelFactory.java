a461085ec8c52c09add68cca77f87c4385e0ae99
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2025-06-04 21:46:59 +0200

    implementado ViewModelFactory

--- app/src/main/java/com/example/appstopsmoke/viewmodel/ViewModelFactory.java
+++ app/src/main/java/com/example/appstopsmoke/viewmodel/ViewModelFactory.java
@@ -1,4 +1,28 @@
 package com.example.appstopsmoke.viewmodel;
 
-public class ViewModelFactory {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.appstopsmoke.data.repository.SmokeRepository;
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+    private final SmokeRepository repository;
+
+    public ViewModelFactory(SmokeRepository repository) {
+        this.repository = repository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(MainViewModel.class)) {
+            return (T) new MainViewModel(repository);
+        } else if (modelClass.isAssignableFrom(StatsViewModel.class)) {
+            return (T) new StatsViewModel(repository);
+        } else if (modelClass.isAssignableFrom(CompareViewModel.class)) {
+            return (T) new CompareViewModel(repository);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class");
+    }
 }
