16affafcff49fb1f2df17bd0c3d8cc0bd5b35af9
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2025-06-04 21:44:06 +0200

    implementado viewmodel comparar

--- app/src/main/java/com/example/appstopsmoke/viewmodel/CompareViewModel.java
+++ app/src/main/java/com/example/appstopsmoke/viewmodel/CompareViewModel.java
@@ -1,4 +1,30 @@
 package com.example.appstopsmoke.viewmodel;
 
-public class CompareViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.appstopsmoke.data.model.Smoke;
+import com.example.appstopsmoke.data.repository.SmokeRepository;
+
+import java.util.List;
+
+public class CompareViewModel extends ViewModel {
+    private final SmokeRepository repository;
+    private final MutableLiveData<List<Smoke>> otherUserData = new MutableLiveData<>();
+
+    // constructor que inyecta la dependencia del repositorio
+    public CompareViewModel(SmokeRepository repository) {
+        this.repository = repository;
+    }
+
+    // carga los datos de otro usuario y los almacena en livedata
+    public void loadOtherUserData(String userId) {
+        repository.getUserSmokes(userId).observeForever(otherUserData::postValue);
+    }
+
+    // devuelve los datos del otro usuario
+    public LiveData<List<Smoke>> getOtherUserData() {
+        return otherUserData;
+    }
 }
